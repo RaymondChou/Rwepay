24833e5280b0bfeb355f0d4f8e30900e9549ee8d
RaymondChou <freezestart@gmail.com>
2014-03-29 20:22:53 +0800

    remove

--- test/test_rwepay.rb
+++ test/test_rwepay.rb
@@ -25,19 +25,19 @@ class TestRwepay < Test::Unit::TestCase
     assert_equal result_json, result_json
   end
 
-  def test_notify_verify
-    params = "xxx"
-
-    configs = {
-        :app_id => 'wxf8b4f85f3a794e77',
-        :partner_id => '1900000109',
-        :app_key => '2Wozy2aksie1puXUBpWD8oZxiD1DfQuEaiC7KcRATv1Ino3mdopKaPGQQ7TtkNySuAmCaDCrw4xhPY5qKTBl7Fzm0RgR3c0WaVYIXZARsxzHV2x7iwPPzOz94dnwPWSn',
-        :partner_key => '8934e7d15453e97507ef794cf7b0519d'
-    }
-
-    payment = Rwepay::JSPayment.new configs
-    assert_equal true, payment.notify_verify?(params)
-  end
+  #def test_notify_verify
+  #  params = "xxx"
+  #
+  #  configs = {
+  #      :app_id => 'wxf8b4f85f3a794e77',
+  #      :partner_id => '1900000109',
+  #      :app_key => '2Wozy2aksie1puXUBpWD8oZxiD1DfQuEaiC7KcRATv1Ino3mdopKaPGQQ7TtkNySuAmCaDCrw4xhPY5qKTBl7Fzm0RgR3c0WaVYIXZARsxzHV2x7iwPPzOz94dnwPWSn',
+  #      :partner_key => '8934e7d15453e97507ef794cf7b0519d'
+  #  }
+  #
+  #  payment = Rwepay::JSPayment.new configs
+  #  assert_equal true, payment.notify_verify?(params)
+  #end
 
 
 end
